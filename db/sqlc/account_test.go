b5c8726c35d06024453d268acd9c93c12188084c
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-07 12:01:25 +0000

    created random generators
    Signed-off-by: GitHub <noreply@github.com>

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,10 +1,30 @@
-package db_test
+package db
 
 import (
+	"context"
 	"testing"
 
+	"github.com/1shubham7/bank/util"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateAccount(t *testing.T){
-	
+func TestCreateAccount(t *testing.T) {
+	assert := assert.New(t)
+	args := CreateAccountParams{
+		Owner:    "Shubham",
+		Balance:  util.RandomBalance(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), args)
+
+	assert.NotNil(account)
+	assert.NoError(err)
+
+	assert.Equal(account.Owner, "Shubham")
+	assert.Equal(account.Balance, int64(100000000000))
+	assert.Equal(account.Currency, "USD")
+
+	assert.NotZero(account.ID)
+	assert.NotZero(account.CreatedAt)
 }
---
+++ util/random.go
@@ -0,0 +1,43 @@
+// we will use this file to generate random names and numbers for test cases
+
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alpha = "abcdefghijklmnopqrstuvwxyz"
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 { 
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+
+	k := 26
+	for i:=0; i<n;i++{
+		alphabet := alpha[rand.Intn(k)]
+		sb.WriteByte(alphabet)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string{
+	return RandomString(10)
+}
+
+func RandomBalance() int64{
+	return RandomInt(10, 10000000000)
+}
+
+func RandomCurrency() string{
+	currencies := []string{"USD", "INR", "CAD", "EUR", "YUAN"}
+	return currencies[rand.Intn(5)]
+}
