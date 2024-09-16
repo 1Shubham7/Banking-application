3d188f2f24dce7adcaeff2e1a5df8ad16b4b0511
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-16 09:58:34 +0000

    added listAccount handler
    Signed-off-by: GitHub <noreply@github.com>

--- api/accounts.go
+++ api/accounts.go
@@ -40,7 +40,7 @@ func (server *Server) createAccount(ctx *gin.Context){
 }
 
 type getAccountRequest struct{
-	ID int64 `uri:"id" binding:"required,min=1`	
+	ID int64 `uri:"id" binding:"required, min=1"`	
 }
 
 func (server *Server) getAccount (ctx *gin.Context){
@@ -64,4 +64,33 @@ func (server *Server) getAccount (ctx *gin.Context){
 	}
 
 	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountRequest struct{
+	PageID int32 `form:"page_id" binding:"required, min=1"`
+	PageSize int32 `form:"page_size" binding:"required, min=3, max=20"`	
+}
+
+func (server *Server) listAccount (ctx *gin.Context){
+	var req listAccountRequest
+
+	err := ctx.ShouldBindQuery(&req)
+	if err != nil{
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit: req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
