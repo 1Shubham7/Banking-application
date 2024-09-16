043678225f186562953e312421b341d9905043fb
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-16 00:21:40 +0000

    writing gin gonic server and handlers
    Signed-off-by: GitHub <noreply@github.com>

---
+++ api/server.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	db "github.com/1shubham7/bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server  struct{
+	store *db.SQLStore
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server{
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	server.router = router
+	return server
+}
