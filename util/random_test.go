e4d60c91bf54461612c9ef7ba20f6e6e6afa1b4c
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-18 01:37:17 +0000

    created tests for random funcs
    Signed-off-by: GitHub <noreply@github.com>

---
+++ util/random_test.go
@@ -0,0 +1,56 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRandomInt(t *testing.T) {
+	assert := assert.New(t)
+
+	min, max := int64(1), int64(100)
+	randomValue := RandomInt(min, max)
+	assert.GreaterOrEqual(randomValue, min)
+	assert.LessOrEqual(randomValue, max)
+}
+
+func TestRandomString(t *testing.T) {
+	assert := assert.New(t)
+
+	length := 10
+	randomStr := RandomString(length)
+	assert.Equal(len(randomStr), length)
+
+	for _, char := range randomStr {
+		assert.Contains(alpha, string(char))
+	}
+}
+
+func TestRandomOwner(t *testing.T) {
+	assert := assert.New(t)
+
+	randomOwner := RandomOwner()
+	assert.Equal(len(randomOwner), 10)
+
+	for _, char := range randomOwner {
+		assert.Contains(alpha, string(char))
+	}
+}
+
+func TestRandomBalance(t *testing.T) {
+	assert := assert.New(t)
+
+	min, max := int64(10), int64(10000000000)
+	randomBalance := RandomBalance()
+	assert.GreaterOrEqual(randomBalance, min)
+	assert.LessOrEqual(randomBalance, max)
+}
+
+func TestRandomCurrency(t *testing.T) {
+	assert := assert.New(t)
+
+	currencies := []string{"USD", "INR", "CAD", "EUR", "YUAN"}
+	randomCurrency := RandomCurrency()
+	assert.Contains(currencies, randomCurrency)
+}
