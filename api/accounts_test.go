3410fad1b2b37add0f68604accb9b45863d5d065
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-18 07:59:19 +0000

    added details to getAccount test
    Signed-off-by: GitHub <noreply@github.com>

--- api/accounts_test.go
+++ api/accounts_test.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"encoding/json"
 
 	mockdb "github.com/1shubham7/bank/db/mock"
 	db "github.com/1shubham7/bank/db/sqlc"
@@ -40,4 +43,16 @@ func TestGetAccount(t *testing.T){
 	server.router.ServeHTTP(recorder, req)
 
 	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	requireBodyMatchAccount(t, recorder.Body, account)
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
+	data, err := io.ReadAll(body)
+	assert.NoError(t, err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	assert.NoError(t, err)
+	assert.Equal(t, account, gotAccount)
 }
