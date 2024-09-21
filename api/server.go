d054608d105b2ee6e1232db1a00f73637d09923f
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-21 23:04:03 +0000

    created api for createuser
    Signed-off-by: GitHub <noreply@github.com>

--- api/server.go
+++ api/server.go
@@ -22,6 +22,8 @@ func NewServer(store db.Store) *Server {
 	}
 
 	router.POST("/accounts", server.createAccount)
+	router.POST("/users", server.createAccount)
+
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 
---
+++ api/user.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/1shubham7/bank/db/sqlc"
+	"github.com/1shubham7/bank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username       string `json:"username" binding:"required,alphanum"`
+	HashedPassword string `json:"hashed_password" binding:"required,min=6"`
+	FullName       string `json:"full_name" binding:"required"`
+	Email          string `json:"email" binding:"required,email"`
+}
+
+// let's not return User in createUser rather remove password and then return it, to address security concerns
+type createUserResponse struct{ // User without HashedPassword field
+	Username       string `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (server *Server) CreateUser(ctx *gin.Context) {
+	var req createUserRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashPassword, err := util.HashPassword(req.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	arg := db.CreateUserParams{
+		Username:    req.Username,
+		HashedPassword:  hashPassword,
+		FullName: req.FullName,
+		Email: req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name(){
+			case "unique_voilation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		} 
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	resp := createUserResponse{
+		Username: user.Username,
+		FullName: user.FullName,
+		Email: user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt: user.CreatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, resp)
+}
