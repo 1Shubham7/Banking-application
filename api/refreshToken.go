42c67b7bace4af01797a9091cec39767600bd701
Shubham Singh <shubhammahar1306@gmail.com>
2024-11-09 16:24:54 +0530

    added renew_token route as well
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

---
+++ api/refreshToken.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type renewAccessTokenRequest struct {
+	RefreshToken string `json:"refresh_token" binding:"required"`
+}
+
+type renewAccessTokenResponse struct {
+	AccessToken string       `json:"access_token"`
+	AccessTokenExpiresAt time.Time `json:"access_token_expires_at"`
+}
+
+func (server *Server) renewAccessToken(ctx *gin.Context) {
+	var req renewAccessTokenRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	refreshPayload, err := server.tokenMaker.VerifyToken(req.RefreshToken)
+	if err != nil{
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	session, err := server.store.GetSession(ctx, refreshPayload.ID)
+	if err != nil {
+		//if username is not in database
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	if session.IsBlocked {
+		err := fmt.Errorf("Your session is blocked")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	}
+
+	if session.Username != refreshPayload.Username{
+		err := fmt.Errorf("incorrect session user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	}
+
+	if session.RefreshToken != req.RefreshToken{
+		err := fmt.Errorf("mismatch session  token")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	}
+
+	if time.Now().After(session.ExpiresAt) {
+		err := fmt.Errorf("session has already expired")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		refreshPayload.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	rsp := renewAccessTokenResponse{
+		AccessToken: accessToken,
+		AccessTokenExpiresAt: accessPayload.Expiry,
+	}
+	ctx.JSON(http.StatusOK, rsp)
+}
