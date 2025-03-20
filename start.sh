c4a603da4013a2e89351334c17face10effb4dbd
1Shubham7 <shubhammahar1306@gmail.com>
2025-03-20 09:56:53 +0530

    explaination for code improved
    Signed-off-by: 1Shubham7 <shubhammahar1306@gmail.com>

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# exit if the command returns a non zero status
+# exits the script if any command fails
 set -e
 
 echo "run db migration"
+
+# run database migrations.
 /app/migrate -path /app/migration --database "$DB_SOURCE" --verbose up
 
 echo "start the app"
+
+# execute the command passed to the script
 exec "$@"
